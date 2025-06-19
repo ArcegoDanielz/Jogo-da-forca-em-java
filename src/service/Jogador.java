3d4ab49e1d1de495fa2b625f1ff25cd7d9a5abeb
gui02 <g.amaral@unoesc.edu.br>
2025-06-19 13:49:43 -0300

    feat adicionando a classe jogador ao projeto

--- src/service/Jogador.java
+++ src/service/Jogador.java
@@ -1,4 +1,27 @@
-package service;
-
 public class Jogador {
+    private String nome;
+    private int tentativasRestantes;
+
+    public Jogador(String nome, int tentativas) {
+        this.nome = nome;
+        this.tentativasRestantes = tentativas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTentativasRestantes() {
+        return tentativasRestantes;
+    }
+
+    public void perderTentativa() {
+        if (tentativasRestantes > 0) {
+            tentativasRestantes--;
+        }
+    }
+
+    public boolean aindaTemTentativas() {
+        return tentativasRestantes > 0;
+    }
 }
